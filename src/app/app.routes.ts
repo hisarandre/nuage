0a7cbd4aa2c68e55a8f794538ad8ab7af63e6b92
Sara Andre <hisarandre@gmail.com>
2026-07-10 18:12:11 +0200

    Add login and sign up form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/auth').then(m => m.Auth)
+  },
+  { path: '**', redirectTo: 'login' }
+
+];
