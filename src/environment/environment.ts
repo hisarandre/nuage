751dc65981501350ab1d4c195df2c018a20cbae2
Sara Andre <hisarandre@gmail.com>
2026-07-25 15:27:08 +0200

    Edit env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  SUPABASE_URL: '',
-  SUPABASE_ANON: '',
+  SUPABASE_URL: 'https://dabgsfetkeyraozpafto.supabase.co',
+  SUPABASE_ANON: 'PUBLIC_ANON_KEY'
 };
