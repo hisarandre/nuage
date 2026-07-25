77dddb1c31108cbbe0083a0aec6576be37f0b81e
Sara Andre <hisarandre@gmail.com>
2026-07-25 15:33:47 +0200

    Edit env

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   SUPABASE_URL: 'https://dabgsfetkeyraozpafto.supabase.co',
-  SUPABASE_ANON: 'PUBLIC_ANON_KEY'
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYmdzZmV0a2V5cmFvenBhZnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjIzMTYsImV4cCI6MjA5OTc5ODMxNn0.o3cudJE2vIuKEQSgKAykteUN0IW6k2eWLiKSdykdOiU'
 };
